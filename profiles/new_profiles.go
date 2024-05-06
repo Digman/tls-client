63f614abd8295c31c245cdc4393f70c60a419684
Digman <long2513@gmail.com>
2024-05-06 09:50:10 +0800

    chrome124

--- profiles/new_profiles.go
+++ profiles/new_profiles.go
@@ -199,6 +199,86 @@ var Chrome_120_PSK = NewChromeProfileNonStreams(tls.ClientHelloID{
 				&tls.SCTExtension{},
 				&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
 					{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+					{Group: tls.X25519},
+				}},
+				&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+					tls.GREASE_PLACEHOLDER,
+					tls.X25519,
+					tls.CurveP256,
+					tls.CurveP384,
+				}},
+				&tls.SupportedPointsExtension{SupportedPoints: []byte{
+					tls.PointFormatUncompressed,
+				}},
+				&tls.ApplicationSettingsExtension{SupportedProtocols: []string{"h2"}},
+				&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
+					tls.CertCompressionBrotli,
+				}},
+				&tls.UtlsGREASEExtension{},
+				&tls.UtlsPreSharedKeyExtension{},
+			},
+		}, nil
+	},
+})
+
+var Chrome_124_PSK = NewChromeProfileNonStreams(tls.ClientHelloID{
+	Client:               "Chrome",
+	RandomExtensionOrder: false,
+	Version:              "124_PSK",
+	Seed:                 nil,
+	SpecFactory: func() (tls.ClientHelloSpec, error) {
+		return tls.ClientHelloSpec{
+			CipherSuites: []uint16{
+				tls.GREASE_PLACEHOLDER,
+				tls.TLS_AES_128_GCM_SHA256,
+				tls.TLS_AES_256_GCM_SHA384,
+				tls.TLS_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+			CompressionMethods: []uint8{
+				tls.CompressionNone,
+			},
+			Extensions: []tls.TLSExtension{
+				&tls.UtlsGREASEExtension{},
+				&tls.SNIExtension{},
+				&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+					tls.PskModeDHE,
+				}},
+				&tls.SupportedVersionsExtension{Versions: []uint16{
+					tls.GREASE_PLACEHOLDER,
+					tls.VersionTLS13,
+					tls.VersionTLS12,
+				}},
+				&tls.StatusRequestExtension{},
+				&tls.ExtendedMasterSecretExtension{},
+				&tls.SessionTicketExtension{},
+				&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+					tls.ECDSAWithP256AndSHA256,
+					tls.PSSWithSHA256,
+					tls.PKCS1WithSHA256,
+					tls.ECDSAWithP384AndSHA384,
+					tls.PSSWithSHA384,
+					tls.PKCS1WithSHA384,
+					tls.PSSWithSHA512,
+					tls.PKCS1WithSHA512,
+				}},
+				&tls.RenegotiationInfoExtension{Renegotiation: tls.RenegotiateOnceAsClient},
+				&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1"}},
+				tls.BoringGREASEECH(),
+				&tls.SCTExtension{},
+				&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+					{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
 					{Group: tls.X25519Kyber768Draft00},
 					{Group: tls.X25519},
 				}},
